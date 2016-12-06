392f2d0f1783d69c6135fa1c76dcce68ac507bfa
sofiane ouafir <sofiane.ouafir@live.fr>
2016-12-06 09:38:17 +0100

    j'ai refais un commit du premier commit car j'avais un problème avec le visualiseur

--- bataille-navale/pageJeu.xaml.cs
+++ bataille-navale/pageJeu.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace bataille_navale
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class pageJeu : Page
     {
-        public MainPage()
+        public pageJeu()
         {
             this.InitializeComponent();
         }
